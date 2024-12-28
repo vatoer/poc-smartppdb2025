90874dfec0a9907a7a5c97b886fe7de25fcaaed2
git uploader <abrary@basic41.id>
2024-12-28 14:16:38 +0100

    fix add nextjs with shared ui

--- apps/frontend/src/components/next-theme.tsx
+++ apps/frontend/src/components/next-theme.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function NextTheme({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
